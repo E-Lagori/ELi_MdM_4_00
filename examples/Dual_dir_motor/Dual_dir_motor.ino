e7f7f9951e48be28062629eb82dbe2740e788195
E-LAGORi Prototype your dreams <contact@e-lagori.com>
2022-09-08 20:29:24 +0530

    Update Dual_dir_motor.ino

--- examples/Dual_dir_motor/Dual_dir_motor.ino
+++ examples/Dual_dir_motor/Dual_dir_motor.ino
@@ -4,8 +4,8 @@
 struct MD3_6_4_00_Pinconfig p1 = {.IN1 = CtrlBUS_AE,.IN2 = CtrlBUS_AF,.VREF = 0};
 struct MD3_6_4_00_Pinconfig p2 = {.IN1 = CtrlBUS_AC,.IN2 = CtrlBUS_AD,.VREF = 0};
 
-MD3_6_4_00_DD test3(p2, 100,MCPWM_UNIT_0,MCPWM_TIMER_0);
-MD3_6_4_00_DD test4(p1, 100,MCPWM_UNIT_1,MCPWM_TIMER_0);
+MD3_6_4_00_DD test3(p2, 100, 1,MCPWM_UNIT_0,MCPWM_TIMER_0);
+MD3_6_4_00_DD test4(p1, 100,1, MCPWM_UNIT_1,MCPWM_TIMER_0);
 
 void setup() {
   // put your setup code here, to run once:
@@ -16,20 +16,20 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  test3.setspeed(80,0);
+  test3.setspeed_lin(80,0);
   delay(100);
-  test3.setspeed(0,0);
+  test3.setspeed_lin(0,0);
   
-  test3.setspeed(50,1);
+  test3.setspeed_lin(50,1);
   delay(100);
-  test3.setspeed(0,1);
+  test3.setspeed_lin(0,1);
   
-  test4.setspeed(80,0);
+  test4.setspeed_lin(80,0);
   delay(100);
-  test4.setspeed(0,0);
+  test4.setspeed_lin(0,0);
   
-  test4.setspeed(50,1);
+  test4.setspeed_lin(50,1);
   delay(100);
-  test4.setspeed(0,1);
+  test4.setspeed_lin(0,1);
 
 }
